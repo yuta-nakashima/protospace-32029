b257c022c938e2877903a733aaaddfd3fa648bac
yuta-nakashima <73780934+yuta-nakashima@users.noreply.github.com>
2020-11-19 18:12:32 +0900

    リダイレクト・コメント名編集

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show , :edit]
   before_action :set_prototype, only: [ :show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
 
